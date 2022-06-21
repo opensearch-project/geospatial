52831dc89c1369a306c477a2ca3c4b4be702d11e
Navneet Verma <navneev@amazon.com>
2022-06-20 19:33:47 -0700

    Enabled Lombok in the repo for adding the boiler-plate code.
    Signed-off-by: Navneet Verma <navneev@amazon.com>

--- src/main/java/org/opensearch/geospatial/shared/URLBuilder.java
+++ src/main/java/org/opensearch/geospatial/shared/URLBuilder.java
@@ -5,17 +5,18 @@
 
 package org.opensearch.geospatial.shared;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
 /**
  * Helper to build url path for this plugin
  */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class URLBuilder {
 
     public static final String NAME = "geospatial";
     public static final String PLUGIN_PREFIX = "_plugins";
     public static final String URL_DELIMITER = "/";
-
-    // Avoid instantiation of this class
-    private URLBuilder() {}
 
     /**
      * @return plugin URL prefix path for {@link org.opensearch.geospatial.plugin.GeospatialPlugin}
