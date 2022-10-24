efeff96e6a314624aeb74fe65669ff653bf4633e
Vijayan Balasubramanian <balasvij@amazon.com>
2022-10-24 15:15:47 -0700

    Support Uber's H3 geospatial indexing system as geohex_grid (#179)
    * Import h3 library (#154)
    
    Made following changes to make it compatible:
    1. Rename package from elasticsearch to opensearch.geospatial
    2. Update License headers
    3. Update build file
    4. Update settings to include sub projects
    
    * Use Transport Request (#164)
    
    Remove usage of deprecated BaseNodeRequest
    
    * Update http client package to resolve build failure (#168) (#171)
    
    * Introduce H3 min resolution constant (#165)
    
    H3 version 1 has 16 resolutions, numbered 0 through 15.
    Introduced a constant to represent min value, similar
    to max value.
    
    * Add geohex aggregation (#160)
    
    This aggregation will use uber's h3 to group coordinates into H3 cell.
    Created new aggregation type geohex_grid. The precision will be between
    0 and 15. This aggreation has default precision as 5,
    similar to geohash and geotile.
    
    Signed-off-by: Vijayan Balasubramanian <balasvij@amazon.com>
    
    * Add integration test (#176)
    
    Included integration test for geohex_grid.
    
    Signed-off-by: Vijayan Balasubramanian <balasvij@amazon.com>

---
+++ src/main/java/org/opensearch/geospatial/search/aggregations/bucket/geogrid/GeoHexGridAggregator.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.geospatial.search.aggregations.bucket.geogrid;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import org.opensearch.geo.search.aggregations.bucket.geogrid.BaseGeoGridBucket;
+import org.opensearch.geo.search.aggregations.bucket.geogrid.GeoGridAggregator;
+import org.opensearch.search.aggregations.Aggregator;
+import org.opensearch.search.aggregations.AggregatorFactories;
+import org.opensearch.search.aggregations.CardinalityUpperBound;
+import org.opensearch.search.aggregations.support.ValuesSource;
+import org.opensearch.search.internal.SearchContext;
+
+/**
+ * Aggregates data expressed as H3 Cell ID.
+ */
+public class GeoHexGridAggregator extends GeoGridAggregator<GeoHexGrid> {
+
+    public GeoHexGridAggregator(
+        String name,
+        AggregatorFactories factories,
+        ValuesSource.Numeric valuesSource,
+        int requiredSize,
+        int shardSize,
+        SearchContext aggregationContext,
+        Aggregator parent,
+        CardinalityUpperBound cardinality,
+        Map<String, Object> metadata
+    ) throws IOException {
+        super(name, factories, valuesSource, requiredSize, shardSize, aggregationContext, parent, cardinality, metadata);
+    }
+
+    @Override
+    protected GeoHexGrid buildAggregation(String name, int requiredSize, List<BaseGeoGridBucket> buckets, Map<String, Object> metadata) {
+        return new GeoHexGrid(name, requiredSize, buckets, metadata);
+    }
+
+    @Override
+    protected BaseGeoGridBucket newEmptyBucket() {
+        return new GeoHexGridBucket(0, 0, null);
+    }
+}
