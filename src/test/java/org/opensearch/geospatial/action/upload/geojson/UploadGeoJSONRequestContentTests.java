d7656cf5f572f812ee9fe591286277bd722c4e42
Vijayan Balasubramanian <balasvij@amazon.com>
2023-02-08 17:48:55 -0800

    Add limit to geojson upload API
    Will limit upto 10_000 features to upload.
    
    Signed-off-by: Vijayan Balasubramanian <balasvij@amazon.com>

--- src/test/java/org/opensearch/geospatial/action/upload/geojson/UploadGeoJSONRequestContentTests.java
+++ src/test/java/org/opensearch/geospatial/action/upload/geojson/UploadGeoJSONRequestContentTests.java
@@ -10,6 +10,7 @@ import static org.opensearch.geospatial.GeospatialObjectBuilder.randomGeoJSONFea
 import static org.opensearch.geospatial.GeospatialTestHelper.randomLowerCaseString;
 import static org.opensearch.geospatial.action.upload.geojson.UploadGeoJSONRequestContent.FIELD_DATA;
 import static org.opensearch.geospatial.action.upload.geojson.UploadGeoJSONRequestContent.GEOSPATIAL_DEFAULT_FIELD_NAME;
+import static org.opensearch.geospatial.action.upload.geojson.UploadGeoJSONRequestContent.MAX_SUPPORTED_GEOJSON_FEATURE_COUNT;
 
 import java.util.Collections;
 import java.util.Map;
@@ -19,6 +20,7 @@ import org.json.JSONObject;
 import org.opensearch.test.OpenSearchTestCase;
 
 public class UploadGeoJSONRequestContentTests extends OpenSearchTestCase {
+    private static int MIN_FEATURE_COUNT = 3;
     private String indexName;
     private String fieldName;
 
@@ -29,21 +31,21 @@ public class UploadGeoJSONRequestContentTests extends OpenSearchTestCase {
         fieldName = randomLowerCaseString();
     }
 
-    private Map<String, Object> buildRequestContent(String indexName, String fieldName) {
+    private Map<String, Object> buildRequestContent(String indexName, String fieldName, int count) {
         final var contents = new JSONObject();
         contents.put(UploadGeoJSONRequestContent.FIELD_INDEX.getPreferredName(), indexName);
         contents.put(UploadGeoJSONRequestContent.FIELD_GEOSPATIAL.getPreferredName(), fieldName);
         contents.put(UploadGeoJSONRequestContent.FIELD_GEOSPATIAL_TYPE.getPreferredName(), "geo_shape");
         JSONArray values = new JSONArray();
-        values.put(randomGeoJSONFeature(buildProperties(Collections.emptyMap())));
-        values.put(randomGeoJSONFeature(buildProperties(Collections.emptyMap())));
-        values.put(randomGeoJSONFeature(buildProperties(Collections.emptyMap())));
+        for (int i = 0; i < count; i++) {
+            values.put(randomGeoJSONFeature(buildProperties(Collections.emptyMap())));
+        }
         contents.put(FIELD_DATA.getPreferredName(), values);
         return contents.toMap();
     }
 
     public void testCreate() {
-        Map<String, Object> contents = buildRequestContent(indexName, fieldName);
+        Map<String, Object> contents = buildRequestContent(indexName, fieldName, MIN_FEATURE_COUNT);
         final var content = UploadGeoJSONRequestContent.create(contents);
         assertNotNull(content);
         assertEquals(fieldName, content.getFieldName());
@@ -54,19 +56,32 @@ public class UploadGeoJSONRequestContentTests extends OpenSearchTestCase {
     public void testCreateEmptyIndexName() {
         IllegalArgumentException invalidIndexName = assertThrows(
             IllegalArgumentException.class,
-            () -> UploadGeoJSONRequestContent.create(buildRequestContent("", "location"))
+            () -> UploadGeoJSONRequestContent.create(buildRequestContent("", "location", MIN_FEATURE_COUNT))
         );
         assertTrue(invalidIndexName.getMessage().contains("[ index ] cannot be empty"));
     }
 
+    public void testCreateWithOneMoreThanMaxSupportedFeatureCount() {
+        int featureCount = MAX_SUPPORTED_GEOJSON_FEATURE_COUNT + 1;
+        IllegalArgumentException reachedMaxFeatureCount = assertThrows(
+            IllegalArgumentException.class,
+            () -> UploadGeoJSONRequestContent.create(buildRequestContent(indexName, fieldName, featureCount))
+        );
+        assertEquals(
+            "wrong error returned",
+            reachedMaxFeatureCount.getMessage(),
+            "Received 10001 features, but, cannot upload more than 10000 features"
+        );
+    }
+
     public void testCreateEmptyGeospatialFieldName() {
-        final var content = UploadGeoJSONRequestContent.create(buildRequestContent(randomLowerCaseString(), ""));
+        final var content = UploadGeoJSONRequestContent.create(buildRequestContent(randomLowerCaseString(), "", MIN_FEATURE_COUNT));
         assertNotNull(content);
         assertEquals("wrong field name", GEOSPATIAL_DEFAULT_FIELD_NAME, content.getFieldName());
     }
 
     public void testCreateEmptyGeospatialFieldType() {
-        Map<String, Object> contents = buildRequestContent(indexName, fieldName);
+        Map<String, Object> contents = buildRequestContent(indexName, fieldName, MIN_FEATURE_COUNT);
         contents.remove(UploadGeoJSONRequestContent.FIELD_GEOSPATIAL_TYPE.getPreferredName());
         IllegalArgumentException invalidIndexName = assertThrows(
             IllegalArgumentException.class,
