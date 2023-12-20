1fb2da2aee328364bf187dbc4594a1fd7a4c6dee
Ryan Bogan <rbogan@amazon.com>
2023-12-20 11:33:08 -0800

    Upgrade gradle to 8.4
    Signed-off-by: Ryan Bogan <rbogan@amazon.com>

--- src/test/java/org/opensearch/geospatial/plugin/GeospatialPluginTests.java
+++ src/test/java/org/opensearch/geospatial/plugin/GeospatialPluginTests.java
@@ -203,6 +203,7 @@ public class GeospatialPluginTests extends OpenSearchTestCase {
             null,
             ingestService,
             client,
+            null,
             null
         );
     }
