ff7b8c1019915d2c2da1d66840957e924077c121
Andy Kwok <andy.kwok@improving.com>
2024-12-09 13:25:03 -0800

    [Feature] New module `geospatial-client` for cross-plugin IP enrichment  (#700)
    * Common module
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * POC
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * Consolidate interface
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * Consolidate interface
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * Remove common module
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * Gradle cleanup
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * Lombok
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * Java doc - 1
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * Java doc
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * API Signature
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * Fetch default data source
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * Logger
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * Initial test cases
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * Add test-cases
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * Test-cases: Client
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * Update tests
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * Update code style
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * Style fix
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * Update deps
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * Spotless
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * Spotless
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * Code refactor
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * Serialisation attempt
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * Remove custom serialisation support
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * Address code comments
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * Update test infra
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * Remove unused test-cases
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * Add lombok config
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * Update test
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * Update changelog
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * Update gradle
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * Update Gradle build
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * Update artifact info
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * Minimise diff
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * Style fix
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * Code comments
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    ---------
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    (cherry picked from commit eb8aba6fe653a072e1b68fe62ed4d01cc2592ba0)

---
+++ client/src/main/java/org/opensearch/geospatial/action/IpEnrichmentRequest.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.geospatial.action;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+
+import org.opensearch.action.ActionRequest;
+import org.opensearch.action.ActionRequestValidationException;
+import org.opensearch.core.common.io.stream.InputStreamStreamInput;
+import org.opensearch.core.common.io.stream.OutputStreamStreamOutput;
+import org.opensearch.core.common.io.stream.StreamInput;
+import org.opensearch.core.common.io.stream.StreamOutput;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j2;
+
+/**
+ * Wrapper for the IP 2 GeoLocation action request.
+ */
+@Getter
+@Setter
+@Log4j2
+@AllArgsConstructor
+public class IpEnrichmentRequest extends ActionRequest {
+
+    private String ipString;
+
+    private String datasourceName;
+
+    /**
+     * Constructor for TransportAction.
+     * @param streamInput the streamInput.
+     */
+    public IpEnrichmentRequest(StreamInput streamInput) throws IOException {
+        super(streamInput);
+        ipString = streamInput.readString();
+        datasourceName = streamInput.readString();
+        log.trace("Constructing IP Enrichment request with values: [{}, {}]", ipString, datasourceName);
+    }
+
+    /**
+     * Perform validation on the request, before GetSpatial processing it.
+     * @return Exception which contain validation errors, if any.
+     */
+    @Override
+    public ActionRequestValidationException validate() {
+        ActionRequestValidationException errors = new ActionRequestValidationException();
+        if (ipString == null) {
+            errors.addValidationError("ip string should not be null");
+        }
+        if (datasourceName == null) {
+            errors.addValidationError("DateSource should not be null");
+        }
+        return errors.validationErrors().isEmpty() ? null : errors;
+    }
+
+    /**
+     * Overridden method to populate object's payload into StreamOutput form.
+     * @param out the StreamOutput object.
+     * @throws IOException If given StreamOutput is not compatible.
+     */
+    @Override
+    public void writeTo(StreamOutput out) throws IOException {
+        super.writeTo(out);
+        out.writeString(ipString);
+        out.writeString(datasourceName);
+    }
+
+    /**
+     * Static method get around the cast exception happen for cross plugin communication.
+     * @param actionRequest A casted-up version of IpEnrichmentRequest.
+     * @return IpEnrichmentRequest object which can be used within the scope of the caller.
+     */
+    public static IpEnrichmentRequest fromActionRequest(ActionRequest actionRequest) {
+        // From the same classloader
+        if (actionRequest instanceof IpEnrichmentRequest) {
+            return (IpEnrichmentRequest) actionRequest;
+        }
+
+        // Or else convert it
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream(); OutputStreamStreamOutput osso = new OutputStreamStreamOutput(baos)) {
+            actionRequest.writeTo(osso);
+            try (StreamInput input = new InputStreamStreamInput(new ByteArrayInputStream(baos.toByteArray()))) {
+                return new IpEnrichmentRequest(input);
+            }
+        } catch (IOException e) {
+            throw new UncheckedIOException("Failed to parse ActionRequest into IpEnrichmentRequest", e);
+        }
+    }
+}
---
+++ client/src/test/java/org/opensearch/geospatial/action/IpEnrichmentActionClientTests.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.geospatial.action;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.opensearch.client.node.NodeClient;
+import org.opensearch.common.action.ActionFuture;
+import org.opensearch.core.action.ActionResponse;
+
+import lombok.SneakyThrows;
+
+@RunWith(MockitoJUnitRunner.class)
+public class IpEnrichmentActionClientTests {
+
+    @Mock
+    private NodeClient mockNodeClient;
+
+    @Mock
+    private ActionFuture<ActionResponse> mockResult;
+
+    String dummyIpString = "192.168.1.1";
+
+    String dummyDataSourceName = "testDataSource";
+
+    Map<String, Object> dummyPayload = Map.of("k1", "v1");
+
+    @SneakyThrows
+    @Test
+    public void testWithValidResponse() {
+        when(mockResult.get()).thenReturn(new IpEnrichmentResponse(dummyPayload));
+        when(mockNodeClient.execute(eq(IpEnrichmentAction.INSTANCE), any())).thenReturn(mockResult);
+        IpEnrichmentActionClient ipClient = new IpEnrichmentActionClient(mockNodeClient);
+        Map<String, Object> actualPayload = ipClient.getGeoLocationData(dummyIpString, dummyDataSourceName);
+        Assert.assertEquals(dummyPayload, actualPayload);
+    }
+
+    @SneakyThrows
+    @Test(expected = ExecutionException.class)
+    public void testWithException() {
+        when(mockResult.get()).thenThrow(new ExecutionException(new Throwable()));
+        when(mockNodeClient.execute(eq(IpEnrichmentAction.INSTANCE), any())).thenReturn(mockResult);
+        IpEnrichmentActionClient ipClient = new IpEnrichmentActionClient(mockNodeClient);
+        ipClient.getGeoLocationData(dummyIpString, dummyDataSourceName);
+    }
+}
